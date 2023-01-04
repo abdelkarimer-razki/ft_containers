efcc79cb278f4dadd641b57eaab0bd2a06dc2121
ABDELKARIM ER-RAZKI <bilrim.errazki@gmail.com>
2023-01-05 00:15:54 +0100

    implementing structs 'still can't rotate correctly'

--- headers/redBlackTree.hpp
+++ headers/redBlackTree.hpp
@@ -6,7 +6,7 @@
 /*   By: aer-razk <aer-razk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 09:07:25 by aer-razk          #+#    #+#             */
-/*   Updated: 2023/01/04 23:22:00 by aer-razk         ###   ########.fr       */
+/*   Updated: 2023/01/05 00:13:03 by aer-razk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,25 @@ template <typename K, typename V>
 class	redBlackTree
 {
 	private:
-		K				key;
-		V				value;
-		
-		bool			red;
-		redBlackTree	*parent;//using parent pointer so we can recheck
-		redBlackTree	*left;
-		redBlackTree	*root;
-		redBlackTree	*right;
-	public:
-		redBlackTree():key(), value(), right(nullptr), left(nullptr), parent(NULL), red(true){};
-		redBlackTree(K key, V value, bool red, redBlackTree *parent):key(key), value(value), parent(parent), right(NULL), left(NULL), red(red) {
-			if (parent != NULL)
-				root = parent->root;
+		struct Node {
+			K		key;
+			V		value;
+			bool	red;
+			Node *left, *right, *parent;
+
+			Node(K key, V value, bool red, Node *parent) :
+				key(key), value(value), red(red), left(nullptr), right(nullptr), parent(parent) {}
 		};
+		Node	*root;
+	public:
+		redBlackTree():root(NULL){};
 		//this function is used to insert data as BST for now
 		void	insert(K key, V value)
 		{
-			redBlackTree	*tmp = root;
-			if (!tmp->key)//initialising the rootNode
+			Node	*tmp = root;
+			if (!tmp)//initialising the rootNode
 			{
-				root = new redBlackTree(key, value, false, NULL);
+				root = new Node(key, value, false, NULL);
 				return ;
 			}
 			while (tmp)//looping on the current node
@@ -47,7 +45,7 @@ class	redBlackTree
 				{
 					if (!tmp->right)
 					{
-						tmp->right = new redBlackTree(key, value, true, tmp);
+						tmp->right = new Node(key, value, true, tmp);
 						std::cout << "right insertion\n";
 						rotateAndRecolor(tmp->right);
 						break;
@@ -58,7 +56,7 @@ class	redBlackTree
 				{
 					if (!tmp->left)
 					{
-						tmp->left = new redBlackTree(key, value, true, tmp);
+						tmp->left = new Node(key, value, true, tmp);
 						std::cout << "left insertion\n";
 						rotateAndRecolor(tmp->left);
 						break;
@@ -68,16 +66,15 @@ class	redBlackTree
 			}
 		}
 		//this function to recheck
-		void	rotateAndRecolor(redBlackTree *node)
+		void	rotateAndRecolor(Node *tmp_node)
 		{
-			redBlackTree *parent = node->parent;
-			if (node != root && parent->red)
+			Node *parent = tmp_node->parent;
+			if (tmp_node != root && parent->red)
 			{
-				redBlackTree *grandParent = parent->parent;
-				redBlackTree *uncle = (parent == grandParent->left) ? grandParent->right : grandParent->left;
+				Node *grandParent = parent->parent;
+				Node *uncle = (parent == grandParent->left) ? grandParent->right : grandParent->left;
 				if (uncle && uncle->red)
 				{
-					std::cout << "here\n";
 					changeColor(parent);
 					changeColor(uncle);	
 					changeColor(grandParent);
@@ -85,71 +82,69 @@ class	redBlackTree
 				}
 				else if (parent == grandParent->left)
 				{
-					if (node != grandParent->left)
+					if (tmp_node != parent->left)
 						rotateLeft(parent);
 					changeColor(parent);
 					changeColor(grandParent); 
 					rotateRight(grandParent);
-					rotateAndRecolor((node == grandParent->left) ? parent : grandParent);
+					rotateAndRecolor((tmp_node == parent->left) ? parent : grandParent);
 				}
 				else if (parent != grandParent->left)
 				{
-					if (node == grandParent->left)
+					if (tmp_node == parent->left)
 						rotateRight(parent);
 					changeColor(parent);
 					changeColor(grandParent);
 					rotateLeft(grandParent);
-					rotateAndRecolor((node == grandParent->left) ? grandParent : parent);
+					rotateAndRecolor((tmp_node == parent->left) ? grandParent : parent);
 				}
 			}
 		}
 		
-		void	rotateLeft(redBlackTree *node)
+		void	rotateLeft(Node *tmp_node)
 		{
-			redBlackTree *rightNode = node->right;
-			node->right = rightNode->left;
-			if (node->right)
-				node->right->parent = node;
-			rightNode->left = node;
-			rightNode->parent = node->parent;
-			if (!node->parent)
+			Node *rightNode = tmp_node->right;
+			tmp_node->right = rightNode->left;
+			if (tmp_node->right)
+				tmp_node->right->parent = tmp_node;
+			rightNode->left = tmp_node;
+			rightNode->parent = tmp_node->parent;
+			if (!tmp_node->parent)
 				root = rightNode;
-			else if (node->parent->right == node)
-				node->parent->right = rightNode;
+			else if (tmp_node->parent->left == tmp_node)
+				tmp_node->parent->left = rightNode;
 			else
-				node->parent->left = rightNode;
-			node->parent = rightNode;
+				tmp_node->parent->right = rightNode;
+			tmp_node->parent = rightNode;
 		}
 
-		void	rotateRight(redBlackTree *node)
+		void	rotateRight(Node *tmp_node)
 		{
-			redBlackTree *leftNode = node->left;
-			node->left = leftNode->right;
-			if (node->left)
-				node->left->parent = node;
-			leftNode->right = node;
-			leftNode->parent = node->parent;
-			if (!node->parent)
+			Node *leftNode = tmp_node->left;
+			tmp_node->left = leftNode->right;
+			if (tmp_node->left)
+				tmp_node->left->parent = tmp_node;
+			leftNode->right = tmp_node;
+			leftNode->parent = tmp_node->parent;
+			if (!tmp_node->parent)
 				root = leftNode;
-			else if (node->parent->left == node)
-				node->parent->left = leftNode;
+			else if (tmp_node->parent->left == tmp_node)
+				tmp_node->parent->left = leftNode;
 			else
-				node->parent->right = leftNode;
-			node->parent = leftNode;
+				tmp_node->parent->right = leftNode;
+			tmp_node->parent = leftNode;
 		}
 		//this function just to test the insertion
 		void	printFirstNode()
 		{
 			std::cout << "root:" << this->root->key << "| red?:" << this->root->red << std::endl;
-			std::cout << "root:" << this->root->right->key << "| red?:" << this->root->right->red << std::endl;
-			/*std::cout << "left:" << this->left->key << "| red?:" << this->left->red  << std::endl;
-			std::cout << "right:" << this->right->key << "| red?:" << this->right->red  << std::endl;
-			std::cout << "right-left:" << this->right->left->key << "| red?:" << this->right->left->red  << std::endl;*/
+			//std::cout << "root:" << this->root->right->key << "| red?:" << this->root->right->red << std::endl;
 		}
 		//this function to change the color of a node;
-		void	changeColor(redBlackTree *node)
+		void	changeColor(Node *node)
 		{
-			node->red? node->red = false : node->red = true;
+			if (node != root)
+				node->red? node->red = false : node->red = true;
 		}
 };
 
