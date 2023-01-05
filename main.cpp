ed46812c904e9946511f3d9803176f5b3388ee31
ABDELKARIM ER-RAZKI <bilrim.errazki@gmail.com>
2023-01-05 12:21:51 +0100

    implementing structs 'still can't rotate correctly'

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: aer-razk <aer-razk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 09:32:57 by aer-razk          #+#    #+#             */
-/*   Updated: 2023/01/04 23:48:57 by aer-razk         ###   ########.fr       */
+/*   Updated: 2023/01/05 12:21:50 by aer-razk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 int main()
 {
 	ft::map<int, std::string> karim;
-	karim.insert(std::pair<int, std::string>(10, "ten"));
-	karim.insert(std::pair<int, std::string>(7, "seven"));
-	karim.insert(std::pair<int, std::string>(18, "eighteen"));
-	karim.insert(std::pair<int, std::string>(15, "eighteen"));
-	karim.insert(std::pair<int, std::string>(16, "eighteen"));
+	karim.insert(std::pair<int, std::string>(7, "ten"));
+	karim.insert(std::pair<int, std::string>(15, "seven"));
+	karim.insert(std::pair<int, std::string>(8, "eighteen"));
+	/*karim.insert(std::pair<int, std::string>(15, "eighteen"));
+	karim.insert(std::pair<int, std::string>(16, "eighteen"));*/
 	karim.getFirstNode();
 }
